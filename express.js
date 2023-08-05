c2522306b86c3d1b84ff79f09106464ce72b3e34
Kay Hennig <KayHennig98@gmail.com>
2023-08-05 12:24:32 +0200

    port

--- express.js
+++ express.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const https = require('https');
 const app = express();
-const port = 80;
+const port = 8080;
 
 //website
 app.get('/apps/weather', (req, res) => {
